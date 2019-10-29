667e8824990da47ea296605b20e70cee15f6af6f
dlArmgo <alexey_va@outlook.com>
2019-10-29 17:51:41 +0300

    feat: added async ticking mechanism with notifier service

---
+++ Model/NotifierService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TZ.Model
+{
+    public class NotifierService
+    {
+        // Can be called from anywhere
+        public async Task Update(Tetris tetris)
+        {
+            if (Notify != null)
+            {
+                await Notify.Invoke(tetris);
+            }
+        }
+
+        public event Func<Tetris, Task> Notify;
+    }
+}
